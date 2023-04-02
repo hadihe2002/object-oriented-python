7cbd97683675460f53e6954b47df739b5b482ab7
hadi <hadihe2002@gmail.com>
2023-04-02 04:38:46 +0430

    ADD: implementing Car class

---
+++ Car.py
@@ -0,0 +1,13 @@
+class Car:
+  def __init__(self, color, year, is_used, mileage, maximum_speed):
+    self.color = color
+    self.year = year
+    self.is_used = is_used
+    self.mileage = mileage
+    self.maximum_speed = maximum_speed
+
+  def get_price():
+    ...
+
+  def horn():
+    ...
