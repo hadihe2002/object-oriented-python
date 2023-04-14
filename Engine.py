19da7714b2d82300e894c2764fbfbb49a5d7e5ac
hadi <hadihe2002@gmail.com>
2023-04-15 03:38:38 +0430

    add description property to engine and tire

--- Engine.py
+++ Engine.py
@@ -9,5 +9,9 @@ class EngineTypeEnum(Enum):
 
 class Engine:
 
-    def __init__(self, engine_type: EngineTypeEnum):
+    def __init__(self, engine_type: EngineTypeEnum, description: str):
         self.type = engine_type
+        self.description = description
+
+    def __str__(self) -> str:
+        return self.description
--- Tire.py
+++ Tire.py
@@ -1,4 +1,8 @@
 class Tire:
 
-    def __init__(self, tire_type: str):
+    def __init__(self, tire_type: str, description: str):
         self.type = tire_type
+        self.description = description
+
+    def __str__(self) -> str:
+        return self.description
